c7494963731b5235d02093d63b3aabdd0b2353ba
Karim Baali <baali.karim95@gmail.com>
2018-03-10 18:02:53 +0100

    Adding unit test to have a 95+ coverage

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -20,54 +20,63 @@ test('should return a String', () => {
 });
 
 test('should convert an integer', () => {
-  //TOBEFIXED
-  expect(typeof convert(123456789012345, 'Satoshi', 'BTC', 'Number')).toBe("number");
+  expect(convert(123456789012345, 'Satoshi', 'BTC', 'Number')).toEqual(1234567.89012345);
 });
 
 test('should convert a number', () => {
-    expect(typeof convert(1234567.89012345, 'BTC', 'Satoshi', 'Number')).toBe("number");
+  expect(convert(1234567.89012345, 'BTC', 'Satoshi', 'Number')).toEqual(123456789012345);
 });
 
 test('should convert a string', () => {
-  //convert('2', 'BTC', 'BTC', 'Number');
+  expect(convert('2', 'BTC', 'BTC', 'Number')).toEqual(2);
 });
 
 test('should convert a Big number', () => {
-  //convert(new Big(2), 'BTC', 'BTC', 'Number');
+  expect(convert(new Big(2), 'BTC', 'BTC', 'Number')).toEqual(2);
 });
 
 test('should convert a NaN to a Number', () => {
-  //convert(NaN, 'BTC', 'BTC', 'Number');
-  //convert(NaN, 'BTC', 'mBTC', 'Number');
+  expect(typeof convert(NaN, 'BTC', 'BTC', 'Number')).toBe("number");
+  expect(typeof convert(NaN, 'BTC', 'mBTC', 'Number')).toBe("number");
 });
 
 test('should convert a NaN to a String', () => {
-  //convert(NaN, 'BTC', 'BTC', 'String');
-  //convert(NaN, 'BTC', 'mBTC', 'String');
+  expect(typeof convert(NaN, 'BTC', 'BTC', 'String')).toBe("string");
+  expect(typeof convert(NaN, 'BTC', 'mBTC', 'String')).toBe("string");
 });
 
 test('should not convert a NaN to a Big', () => {
-  //convert(NaN, 'BTC', 'BTC', 'Big');
+  expect(() => { convert(NaN, 'BTC', 'BTC', 'Big') }).toThrow();
 });
 
 test('should handle rounding errors', () => {
-  //convert(4.6, 'Satoshi', 'BTC', 'Number');
-  //convert(0.000000046, 'BTC', 'Satoshi', 'Number');
+  expect(convert(4.6, 'Satoshi', 'BTC', 'Number')).toEqual(4.6e-8);
+  expect(convert(0.000000046, 'BTC', 'Satoshi', 'Number')).toEqual(4.6);
 });
 
 test('should throw when untest is undefined', () => {
-  //convert(new Big(2), 'x', 'BTC', 'Number');
-  //convert(new Big(2), 'BTC', 'x', 'Number');
-  //convert(NaN, 'x', 'BTC', 'Number');
-  //convert(NaN, 'BTC', 'x', 'Number');
+  expect(() => {convert(new Big(2), 'x', 'BTC', 'Number')}).toThrow();
+  expect(() => {convert(new Big(2), 'BTC', 'x', 'Number')}).toThrow();
+  expect(() => {convert(NaN, 'x', 'BTC', 'Number')}).toThrow();
+  expect(() => {convert(NaN, 'BTC', 'x', 'Number')}).toThrow();
 });
 
 test('should throw when representaion is undefined', () => {
-  //convert(2, 'BTC', 'mBTC', 'x');
-  //convert(NaN, 'BTC', 'mBTC', 'x');
+  expect(() => {convert(2, 'BTC', 'mBTC', 'x')}).toThrow();
+  expect(() => {convert(NaN, 'BTC', 'mBTC', 'x')}).toThrow();
 });
 
 test('should allow untest aliases', () => {
-  //convert(4.6, 'Satoshi', 'sat');
-  //convert(4.6, 'μBTC', 'btest');
+  expect(convert(4.6, 'Satoshi', 'sat')).toEqual(4.6);
+  expect(() => {convert(4.6, 'μBTC', 'btest')}).toThrow();
+});
+
+test('should allow adding new units', () => {
+  expect(() => {convert.addUnit('BTC', 0.0001)}).toThrow();
+  convert.addUnit('KAR', 10);
+  expect(convert.units()).toContain('KAR');
+});
+
+test('should allow removing units', () => {
+  expect(() => {convert.removeUnit('BTC')}).toThrow();
 });
